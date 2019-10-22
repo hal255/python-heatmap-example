6a3d500882bbd0f28ecaa72626eb93c80e32f788
hal255 <hluu114@gmail.com>
2019-10-21 23:07:21 -0700

    documented more examples

--- python-heatmap-example.py
+++ python-heatmap-example.py
@@ -1,12 +1,22 @@
 # reference: https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/image_annotated_heatmap.html
 
+# other references: 
+# https://matplotlib.org/3.1.1/gallery/recipes/fill_between_alpha.html
+# https://www.youtube.com/watch?v=m7uXFyPN2Sk
+
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 # sphinx_gallery_thumbnail_number = 2
 
+fig = plt.figure()
+
 
 def main():
+    example2()
+
+def example1():
     vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
                 "potato", "wheat", "barley"]
     farmers = ["Farmer Joe", "Upland Bros.", "Smith Gardening",
@@ -43,9 +53,50 @@ def main():
 
     ax.set_title("Harvest of local farmers (in tons/year)")
     fig.tight_layout()
-
+    
+    plt.plot(alpha=0.2)     # adjust alpha
     output_plots()  # save plots to file
+
     plt.show()
+
+def example2():
+    """
+    compute the mean and stddev of 100 data sets and plot mean vs stddev.
+    When you click on one of the mu, sigma points, plot the raw data from
+    the dataset that generated the mean and stddev
+    """
+
+    X = np.random.rand(100, 1000)
+    xs = np.mean(X, axis=1)
+    ys = np.std(X, axis=1)
+
+    # fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title('click on point to plot time series')
+    line, = ax.plot(xs, ys, 'o', picker=5)  # 5 points tolerance
+
+
+def onpick(event):
+
+    if event.artist!=line: return True
+
+    N = len(event.ind)
+    if not N: return True
+
+
+    figi = plt.figure()
+    for subplotnum, dataind in enumerate(event.ind):
+        ax = figi.add_subplot(N,1,subplotnum+1)
+        ax.plot(X[dataind])
+        ax.text(0.05, 0.9, 'mu=%1.3f\nsigma=%1.3f'%(xs[dataind], ys[dataind]),
+                transform=ax.transAxes, va='top')
+        ax.set_ylim(-0.5, 1.5)
+    figi.show()
+    return True
+
+fig.canvas.mpl_connect('pick_event', onpick)
+
+plt.show()    
 
 def output_plots():
     plt.savefig("exported_images/test_output.pdf", dpi=150)
